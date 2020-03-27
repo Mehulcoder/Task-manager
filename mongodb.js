fd95cbc795bd0b5cc9e19b935c27285d58af6078
Mehulcoder <mehul355180@gmail.com>
2020-03-27 21:52:18 +0530

    Learning about _id

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,12 @@
 var mongodb = require('mongodb');
-var MongoClient = mongodb.MongoClient;
+// var MongoClient = mongodb.MongoClient; var ObjectID = mongodb.ObjectID;
+var {MongoClient, ObjectID} = require('mongodb');
 
 var connectionURL = 'mongodb://127.0.0.1:27017';
 var databaseName = 'task-manager';
+
+var id = new ObjectID();
+console.log(id.getTimestamp());
 
 MongoClient.connect(connectionURL,{useNewUrlParser:true, useUnifiedTopology: true},(error, client)=>{
     if(error){
@@ -12,19 +16,17 @@ MongoClient.connect(connectionURL,{useNewUrlParser:true, useUnifiedTopology: tru
     //The database that we are trying to maniplulate
     const db = client.db(databaseName);
 
-    db.collection('tasks').insertMany([{
-        description: "Task 1", 
-        completed: true,
-    },{
-        description: "Task 2", 
-        completed: true,
-    },{
-        description: "Task 3", 
-        completed: false,
-    }],(err, result)=>{
+    db.collection('users').insertOne(
+    {
+        _id: id,
+        name: "Sarthak", 
+        age: 17,
+    },
+    (err, result)=>{
         if (err) {
             return console.log("Unable to insert");
         }
+
         console.log(result.ops);
     });
 });
